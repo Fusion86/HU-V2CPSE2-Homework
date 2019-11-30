58945934ed48e3e7935396e4a8a45764a17fa28c
Fusion86 <fusion86@cerbus.nl>
2019-11-30 17:48:43 +0100

    meh

--- factory/main.cpp
+++ factory/main.cpp
@@ -1,4 +1,26 @@
-int main()
-{
+#include <fstream>
+
+#include "yeet.hpp"
+
+#define WINDOW_SIZE_X 1280
+#define WINDOW_SIZE_Y 720
+#define LEVEL_FILE_NAME "zorlo.txt"
+
+void load_state() {
+    std::ifstream ifs(LEVEL_FILE_NAME);
+    ifs.exceptions(std::ifstream::failbit | std::ifstream::badbit | std::ifstream::eofbit);
+}
+
+void save_state() {
+    std::ofstream ofs(LEVEL_FILE_NAME);
+    ofs.close();
+}
+
+int main() {
+    load_state();
+
+    // Display window and move objects
+
+    save_state();
     return 0;
 }
---
+++ factory/yeet.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <iostream>
+#include <sstream>
+#include <SFML/Graphics.hpp>
+
+class invalid_format_exception : std::exception {
+  public:
+    invalid_format_exception(std::string expected_character) : expected_character(expected_character) {}
+
+    const char* what() noexcept {
+        std::stringstream ss;
+        ss << "Invalid format. Expected '" << expected_character << "' but got something else.";
+        return ss.str().c_str();
+    }
+
+  private:
+    std::string expected_character;
+};
+
+std::istream& operator>>(std::istream& is, sf::Vector2f vec) {
+    char c;
+    is >> c;
+    if (c != '(') throw new invalid_format_exception("(");
+    if (!(is >> vec.x)) throw invalid_format_exception("x coordinate");
+    is >> c;
+    if (c != ',') throw new invalid_format_exception(",");
+    if (!(is >> vec.y)) throw invalid_format_exception("y coordinate");
+    is >> c;
+    if (c != ')') throw new invalid_format_exception(")");
+    return is;
+}
+
+class drawable {
+  public:
+    virtual void draw(sf::RenderWindow& window);
+
+    std::ostream& operator<<(std::ostream& os) { return os; }
+
+    std::istream& operator>>(std::istream& is) { return is; }
+
+  protected:
+    std::string name;
+    sf::Vector2f position;
+};
+
+class selectable {
+  public:
+    bool isSelected;
+};
+
+class circle : public drawable, selectable {};
+
+class rectangle : public drawable, selectable {};
+
+class line : public drawable, selectable {};
+
+class picture : public drawable, selectable {};
